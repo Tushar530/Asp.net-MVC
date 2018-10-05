b856a9cd21718deac08017e098fcafb9e30882bb
Tushar530 <tusharsoni95@gmail.com>
2018-10-05 13:28:42 +0530

    Add files via upload

---
+++ Models/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CODEFIRST.Models
+{
+    [Table(name: "HelloGuys")]
+
+    public class Department
+
+    {
+
+        [Key]
+
+        public int DepartmentID { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+
+
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CODEFIRST.Models
+{
+    [Table(name: "HelloWorld")]
+
+    public class Employee
+
+    {
+
+        [Key]
+
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+
+
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+
+    }
+}
